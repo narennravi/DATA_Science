8f592226f1dcf7eb797451e0c96ead7b5af2f36c
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-04-04 19:13:44 +0530

    Add files via upload

---
+++ 2_MySQL/2.1__Begginer_level/1.3__shows_Table,Database_contents___and___Access_Columns.sql
@@ -0,0 +1,19 @@
+# to use/select database
+use my_sql_practice;
+
+
+# Display us about all tables & Database contents
+show tables;
+show databases;
+
+
+# Display all column-name from table
+select * from my_friends;
+
+
+# Display particular selected columns from table
+select city from my_friends;
+
+
+# Display city - records alone from table: 
+describe my_friends city;
