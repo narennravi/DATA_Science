f5f457aeb409c52a2d7ddddcc303a7d116a5dc13
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-04-04 20:08:47 +0530

    Add files via upload

---
+++ My_SQL/2.1__Beginner-Module/2__Display_Table_in_various-queries.sql
@@ -0,0 +1,15 @@
+# To Select/Use Database
+use my_sql_practice;
+
+
+# Various ways
+# To display "Table" from my Database:-
+
+describe my_friends;
+desc my_friends;
+explain my_friends;
+show columns from my_friends;
+show fields from my_friends;
+
+
+
