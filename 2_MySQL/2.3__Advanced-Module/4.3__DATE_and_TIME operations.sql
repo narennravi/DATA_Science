fee1ead1c6b8ba2d2779fd9d95e89b92501dadac
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-04-10 23:30:42 +0530

    Update 4.3__DATE_and_TIME operations.sql
    Added 3-lines of querry at last --> days-count  &  display of date in particular - format...!

--- 2_MySQL/2.3__Advanced-Module/4.3__DATE_and_TIME operations.sql
+++ 2_MySQL/2.3__Advanced-Module/4.3__DATE_and_TIME operations.sql
@@ -15,3 +15,11 @@ SELECT DATE("2022-09-08 00:10:16");
 #  Only TIME-filtered:-
 SELECT TIME("2022-09-08 00:10:16");
 
+
+#  Based on days-count date will be generated:-
+SELECT from_days('730677');
+
+
+#  Display date with particular format:-
+SELECT DATE_FORMAT(curdate(), '%d/%m/%y');                # Current_date will displayed --> dd/mm/yy/
+SELECT DATE_FORMAT(('2015-09-10'), '%M %D %Y');           # Given date will be displayed as --> September 9th 2010
