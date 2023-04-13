9e45fc60b3eb89ceb8e2918a6f09553d5532fb5d
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-04-13 21:40:25 +0530

    Update 4__How to insert values_and_Sortlist specific gender from table.sql
    Added definition & Syntax for easy references.

--- 2_MySQL/2.1__Beginner-Module/4__How to insert values_and_Sortlist specific gender from table.sql
+++ 2_MySQL/2.1__Beginner-Module/4__How to insert values_and_Sortlist specific gender from table.sql
@@ -1,3 +1,26 @@
+/*
+_______________________________________________________________________________________________________________________________________________________________________________________________
+INSERT Statement :-
+            MySQL INSERT statement is used to insert data in MySQL table within the database.
+	    We can insert single or multiple records using a single query in MySQL.
+
+syntax:
+       INSERT INTO --table_name--  --( field1, field2,...fieldN )--  VALUES  --( value1, value2,...valueN )--;
+_______________________________________________________________________________________________________________________________________________________________________________________________
+SELECT Statement :
+           The MySQL SELECT statement is used to fetch data from the one or more tables in MySQL.
+	   We can retrieve records of all fields or specified fields.
+
+Syntax:
+       SELECT * FROM --table-name-- WHERE --condition--;       #Displays table based on condition
+       SELECT * FROM --table-name--;                           #Displays all columns/entire table 
+       SELECT --col-name-- FROM --table-name--;                #Displays selective column 
+       SELECT count(*) FROM --table-name--;                    #Displays Count 
+       SELECT --col-name(*)-- FROM --table-name--;             #Displays selective column count
+_______________________________________________________________________________________________________________________________________________________________________________________________
+*/
+
+
 # Selecting database & table
 use my_sql_practice;
 select * from my_friends;
