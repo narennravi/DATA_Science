a28dc7056564bf362f41ecb18a0f142fa22839e2
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-04-13 21:18:28 +0530

    Update 3__How to use COUNT__SUM__DISTINCT in table from existing database.sql
    Added Notes + syntax for easy-references.

--- 2_MySQL/2.2__Intermediate-Module/3__How to use COUNT__SUM__DISTINCT in table from existing database.sql
+++ 2_MySQL/2.2__Intermediate-Module/3__How to use COUNT__SUM__DISTINCT in table from existing database.sql
@@ -1,3 +1,35 @@
+/*
+________________________________________________________________________________________________________________________________________________________________________________________
+COUNT :-
+       SQL COUNT function is the simplest function and very useful in counting the number of records, which are expected to be returned by a SELECT statement.
+       This function is used to count the certain amount of values in the column that have been inserted in the table.
+
+Syntax for Count:
+       SELECT count(*) FROM --tablename--;
+________________________________________________________________________________________________________________________________________________________________________________________
+DISTINCT :-
+       SQL DISTINCT keyword is used in conjunction with the SELECT statement to eliminate all the duplicate records and fetching only unique records.
+       There may be a situation when you have multiple duplicate records in a table. 
+       While fetching such records, it makes more sense to fetch only those unique records instead of fetching duplicate records.
+
+Syntax for Distinct:
+        SELECT DISTINCT --column1, column2,.....columnN--  FROM --table_name--  WHERE --condition--;
+________________________________________________________________________________________________________________________________________________________________________________________
+MAXIMUM :
+       Maximum function is used to find maximum value in the certain column in the given table.
+
+Syntax for Maximum :
+        SELECT MAX(--column_name--)  FROM  --table_name--;
+________________________________________________________________________________________________________________________________________________________________________________________
+MINIMUM :
+       Minimum function is used to find minimum value in the certain column in the given table.
+
+Syntax for Minimum :
+        SELECT MIN(--column_name--)  FROM  --table_name--;
+________________________________________________________________________________________________________________________________________________________________________________________
+*/
+
+
 # We created new table = "my_friends_2" in existing database = "my_sql_practice"
 
 use my_sql_practice;
@@ -24,15 +56,15 @@ INSERT INTO my_friends_2 VALUES('Storm','Wonder',NULL,'F','Hollywood','LA',10);
 select * from my_friends_2;
 
 # NOTE:-
-# Duplicate values were considered bcz we didn't use Primary_key - constraint
+# Duplicate values were considered bcz we did not use Primary_key - constraint
 #--------------------------------------------------------------------------------------------------------------------------------------
 
-# COUNT:-      [How to use count along with condition..?]
+ COUNT:-      [How to use count along with condition..?]
 
-# count the data of entire table
+#count the data of entire table
 select count(*) from my_friends_2; 
 
-# count selective data of column (entire table | first_name | sex) from table    
+#count selective data of column (entire table | first_name | sex) from table    
 select count(*), count(first_name), count(sex) from my_friends_2;
 
 # count table using conditional clause - WHERE ---> to display count of "female" from table
